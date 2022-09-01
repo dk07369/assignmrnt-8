e7db69072530690dce6843161724f22b1c48ba14
dk07369 <112017754+dk07369@users.noreply.github.com>
2022-09-01 17:57:40 +0530

    Add files via upload

---
+++ Paharayouwanttoprint.py
@@ -0,0 +1,7 @@
+n=int(input("what number of pahara you want to print..."))
+i=1
+while i<=10:
+    print(n*i,end=" ")
+    i+=1
+
+print()
---
+++ ReverseorderQ5.py
@@ -0,0 +1,6 @@
+i=10
+while i>=1:
+    print(2*i-1,end=" ")
+    i-=1
+
+print()
